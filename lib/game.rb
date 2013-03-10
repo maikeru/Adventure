0f712eeb4c7324ea33c06ffb59662be800d00806
P. Michael Holland <maikeru.horando@gmail.com>
2013-03-10 18:39:35 +0900

    Add game class stub and beginnings of game and room specs

---
+++ lib/game.rb
@@ -0,0 +1,11 @@
+require_relative 'Player'
+require_relative 'Room'
+require_relative 'Parser'
+require_relative 'Item'
+
+class Game
+  def initialize
+    
+  end
+  
+end
---
+++ spec/room_spec.rb
@@ -0,0 +1,11 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe "Room" do
+  context "when given a direction" do
+    it "should return the room in that direction"
+  end
+  
+  context "when given a direction for a non-existent exit" do
+    it "should return an error"
+  end
+end
