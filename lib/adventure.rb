6b8a6b2b0e9b707e062e99978e8c977c73ac5bc3
P. Michael Holland <maikeru.horando@gmail.com>
2013-03-10 20:34:25 +0900

    More cucumber setup and step implementation

---
+++ lib/adventure.rb
@@ -0,0 +1,1 @@
+require 'adventure/game'
---
+++ lib/adventure/game.rb
@@ -0,0 +1,17 @@
+require_relative 'player'
+require_relative 'room'
+require_relative 'parser'
+require_relative 'item'
+
+module Adventure
+  class Game
+    def initialize
+
+    end
+
+    def start
+
+    end
+  end
+
+end
