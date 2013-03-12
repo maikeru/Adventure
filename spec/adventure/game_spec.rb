34ed10d982f8a1109a256a825cc93cd00486b4a6
P. Michael Holland <maikeru.horando@gmail.com>
2013-03-12 19:52:38 +0900

    Move game_spec and update spec_helper to match the RSpec book

---
+++ spec/adventure/game_spec.rb
@@ -0,0 +1,13 @@
+#require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+require 'spec_helper'
+
+module Adventure
+  describe Game do
+    describe "#start" do
+      it "sends a welcome message"
+      it "describes your location"
+      it "prompts for a command"
+    end
+  end
+end
