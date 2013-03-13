27e48178dd8a53085ad9a54cd5abd0d73d2857ac
P. Michael Holland <maikeru.horando@gmail.com>
2013-03-13 21:23:02 +0900

    Add player_enters_command feature and steps

---
+++ features/player_enters_command.feature
@@ -0,0 +1,10 @@
+Feature: Player enters command
+  In order to play the game
+  As a player
+  I want the game to respond to commands
+
+Scenario: enter look command
+  Given the game has started
+  When I enter the "look" command
+  Then I should see "You are in your bedroom. There is a door to the north"
+  And I should see "> "
--- features/step_definitions/game_steps.rb
+++ features/step_definitions/game_steps.rb
@@ -6,10 +6,18 @@ When(/^I start a new game$/) do
   game.start
 end
 
+Given(/^the game has started$/) do
+  game = Adventure::Game.new(output)
+  game.start
+end
+
 Then(/^I should see "(.*?)"$/) do |message|
   output.messages.should include(message)
 end
 
+When(/^I enter the "(.*?)" command$/) do |command|
+  input_handler.content.should include(command)
+end
 
 class Output
   def messages
@@ -23,4 +31,18 @@ end
 
 def output
   @output ||= Output.new
+end
+
+class InputHandler
+  def content
+    @content ||= []
+  end
+  
+  def gets(content)
+    content << content
+  end
+end
+
+def input_handler 
+  @input_handler ||= InputHandler.new
 end
