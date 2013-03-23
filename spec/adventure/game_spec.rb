ecd021130894ecd44dc9232b1df1e5027630f398
P. Michael Holland <maikeru.horando@gmail.com>
2013-03-23 22:59:07 +0900

    refactor game_spec

--- spec/adventure/game_spec.rb
+++ spec/adventure/game_spec.rb
@@ -18,15 +18,21 @@ module Adventure
         game.start
       end
     end
-    
-    describe "handle input" do
+
+    describe "#process_input" do
       let(:output) { double('output').as_null_object }
       let(:game)   { Game.new(output) }
-      describe "look command" do
-        it "describes your location and prompts for the next command" do
+
+      describe "look" do
+        before(:each) do
           bedroom = Room.new(:bedroom, "You are in your bedroom. There is a door to the north")
           game.current_location = bedroom
+        end
+        it "describes your location" do
           output.should_receive(:puts).with("You are in your bedroom. There is a door to the north")
+          game.process_input("look")
+        end
+        it "prompts for the next command" do
           output.should_receive(:puts).with("> ")
           game.process_input("look")
         end
