10f03d676bc336d813781415c16de16674fa741e
P. Michael Holland <maikeru.horando@gmail.com>
2013-03-23 23:18:00 +0900

    refactor room_spec

--- spec/adventure/room_spec.rb
+++ spec/adventure/room_spec.rb
@@ -1,20 +1,29 @@
 require 'spec_helper'
 
 describe "Room" do
+  
   let(:room) { Room.new(:bedroom, "You are in a victorian bedroom. It has a four-poster bed in it.") }
+    
   describe "#show" do
     it "describes the room" do
       room.show.should eql("You are in a victorian bedroom. It has a four-poster bed in it.")
     end
-    
-    it "lists items in the room" do
-      item = Item.new(:book, name: "a book", description: "a leather-bound book with mysterious writing on the cover")
-      room.add_item(item)
-      room.show.should include("You can see:")
-      room.show.should include("  a book")
+
+    context "when there are items in the room" do
+      before(:each) do
+        item = Item.new(:book, name: "a book", description: "a leather-bound book with mysterious writing on the cover")
+        room.add_item(item)
+      end
+      it "displays 'You can see:'" do
+        room.show.should include("You can see:")
+      end
+      
+      it "lists the items" do
+        room.show.should include("  a book")
+      end
     end
   end
-  
+
   describe "item related" do
     describe "#add_item and #remove item" do
       item = Item.new(:book, name: "a book", description: "a leather-bound book with mysterious writing on the cover")
