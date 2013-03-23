71f0b8ac5b9f0d8e8e8888f06c8acf388147887f
P. Michael Holland <maikeru.horando@gmail.com>
2013-03-23 21:47:56 +0900

    Correct input handling spec as it wasn't testing what it should have been

--- spec/adventure/game_spec.rb
+++ spec/adventure/game_spec.rb
@@ -22,18 +22,14 @@ module Adventure
     describe "handle input" do
       let(:output) { double('output').as_null_object }
       let(:game)   { Game.new(output) }
-      it "accepts 'look' as input" do
-        bedroom = Room.new(:bedroom, "You are in your bedroom. There is a door to the north")
-        game.current_location = bedroom
-        game.process_input("look")
-      end
-      it "describes your location" do
-        output.should_receive(:puts).with("You are in your bedroom. There is a door to the north")
-        game.start
-      end
-      it "prompts for a command" do
-        output.should_receive(:puts).with("> ")
-        game.start
+      describe "look command" do
+        it "describes your location and prompts for the next command" do
+          bedroom = Room.new(:bedroom, "You are in your bedroom. There is a door to the north")
+          game.current_location = bedroom
+          output.should_receive(:puts).with("You are in your bedroom. There is a door to the north")
+          output.should_receive(:puts).with("> ")
+          game.process_input("look")
+        end
       end
     end
   end
